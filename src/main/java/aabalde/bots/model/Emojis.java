ce85ef663f88de01c9d3b43e25cfc89ec0a94615
Adrián Abalde <aabalde@gradiant.org>
2018-03-23 10:12:00 +0100

    Added Emojis static class

---
+++ src/main/java/aabalde/bots/model/Emojis.java
@@ -0,0 +1,17 @@
+package aabalde.bots.model;
+
+/**
+ * Created by aabalde on 23/03/18.
+ */
+public class Emojis {
+
+    public static String PERSON_SILOUETTE = "\uD83D" + "\uDC64";
+
+    public static String RIGHT_ARROW = (char)10145 + "" + (char)65039;
+
+    public static String NOTEPAD = "\uD83D" + "\uDCDD";
+
+    public static String PUSHPIN = "\uD83D" + "\uDCCC";
+
+    public static String BLUE_DIAMOND = "\uD83D" + "\uDD39";
+}
